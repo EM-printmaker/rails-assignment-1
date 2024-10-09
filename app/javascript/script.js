fa4cdc2b786f68661f7400017b87dd2c90fb3e0e
江刺家舞 <esashikamai@esashikamainonotobukkukonpyuta.local>
2024-10-10 00:11:38 +0900

    add:bootstrap-datepickerを導入

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -4,3 +4,10 @@ $(function () {
     $(this).animate({opacity: 0}, 200);
   });
 });
+
+  $(document).on('turbolinks:load', function() {
+    $('.datepicker').datepicker({
+      format: "yyyy/mm/dd",
+      language: "ja"
+    });
+  });
